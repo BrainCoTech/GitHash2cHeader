f6f9d76dd56701ad35e1df3d0dfc96ea44f5b0bf
tianhe wang <tianhe.wang@brainco.tech>
2018-02-10 17:53:56 -0500

    re-organize code
    auto create parent folder.

--- src/GitHash2cHeader.java
+++ src/GitHash2cHeader.java
@@ -25,71 +25,89 @@ public class GitHash2cHeader {
 			System.out.printf("Please specify input and output file name");
 			return;
 		}
-		String InputfileName = args[0]+"HEAD";
 		String OutputfileName = args[1];
-		BufferedReader br;
-		BufferedWriter bw;
-		String filecontent;
 		try {
-			bw = new BufferedWriter(new FileWriter(new File(OutputfileName)));
+			String InputfileName = null;
+			String BranchName = null;
+			String CommitHASH = null;
+			try {
+				InputfileName = args[0] + "/HEAD";
+				BufferedReader br;
+				//read git head message
+				br = new BufferedReader(new FileReader(new File(InputfileName)));
+				String filecontent = br.readLine();
+				br.close();
+
+				//get branch name:
+				//FIXME: currently need checkout point has a branch name,
+				//cannot support checkout without any branch
+				String ref_filepath[] = filecontent.split(": ", 2);
+				if(ref_filepath.length != 2) {
+					throw new IOException();
+				}
+				String branchname_arr[] = ref_filepath[1].split("/");
+				BranchName = branchname_arr[branchname_arr.length-1];
+				if(BranchName != null) {
+					System.out.println(BranchName);
+				}
+
+				//get commit hash
+				//get commit hash file path
+				InputfileName = args[0] + "/";
+				InputfileName += ref_filepath[1];
+				br = new BufferedReader(new FileReader(new File(InputfileName)));
+				CommitHASH = br.readLine();
+				if(CommitHASH != null) {
+					System.out.println(CommitHASH);
+	      }
+				br.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+        System.out.println("Unable to open file '" + InputfileName + "'");                
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+        System.out.println("Error reading file '" + InputfileName + "'");
+        throw new IOException();
+			}
+			BufferedWriter bw;
+			File outputfile = new File(OutputfileName);
+			outputfile.getParentFile().mkdirs();
+			bw = new BufferedWriter(new FileWriter(outputfile));
 			//write #ifdef
 			bw.write("#ifndef __GITINFO_H\n#define __GITINFO_H\n");
-			
-			//read git head message
-			br = new BufferedReader(new FileReader(new File(InputfileName)));
-			if((filecontent = br.readLine()) != null) {
-                System.out.println(filecontent);
-            }
-			br.close();
-			String ref_filepath[] = filecontent.split(": ", 2);
-			if(ref_filepath.length != 2) {
-				bw.close();
-				throw new IOException();
-			}
-			//get branch name:
-			//FIXME: currently need checkout point has a branch name,
-			//cannot support checkout without any branch
-			String branchname_arr[] = ref_filepath[1].split("/");
-			bw.write("#define CURRENT_BRANCHNAME \""+branchname_arr[branchname_arr.length-1]+"\"\n");
-			
-			//get commit hash file
-			InputfileName = args[0]+ref_filepath[1];
-			br = new BufferedReader(new FileReader(new File(InputfileName)));
-			if((filecontent = br.readLine()) != null) {
-                System.out.println(filecontent);
-            }
-			//get commit hash
-			bw.write("#define CURRENT_COMMIT_HASH \""+filecontent+"\"\n");
+			bw.write("#define CURRENT_BRANCHNAME \"" + BranchName + "\"\n");
+			bw.write("#define CURRENT_COMMIT_HASH \"" + CommitHASH + "\"\n");
 			
 			//get current runtime as UTC.
 			Date s = new Date();
 			long currentmsec = s.getTime();
-			String utcsec = Integer.toUnsignedString((int)(currentmsec/1000));
 			String utcmsec = Long.toUnsignedString(currentmsec);
+			System.out.println(utcmsec);
+			String utcsec = utcmsec.substring(0, utcmsec.length()-3);
 			
 			//write current runtime string.
-			bw.write("#define BUILD_STEP_UTC_SEC "+utcsec+"UL\n");
-			bw.write("#define BUILD_STEP_UTC_MSEC "+utcmsec+"ULL\n");
+			bw.write("#define BUILD_STEP_UTC_SEC " + utcsec + "UL\n");
+			bw.write("#define BUILD_STEP_UTC_MSEC " + utcmsec + "ULL\n");
 			
 			//write #endif
 			bw.write("#endif\n");
 			bw.flush();
-
-			br.close();
 			bw.close();
 			System.out.printf("Successfully ended\n");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			//e.printStackTrace();
             System.out.println(
-                    "Unable to open file '" + 
-                    InputfileName + "'");                
+                    "Unable to write file '" + 
+                    		OutputfileName + "'");                
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			//e.printStackTrace();
             System.out.println(
                     "Error reading file '" 
-                    + InputfileName + "'");
+                    + OutputfileName + "'");
 		}
 	}
 }
